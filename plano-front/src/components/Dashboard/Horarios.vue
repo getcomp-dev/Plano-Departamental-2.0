1f4172de4a9899fb2d192dec4d90429e19f59043
lucascribeiro <lucascarvalho@ice.ufjf.br>
2018-08-09 20:17:30 -0300

    Geração de tabelas de horários para cc noturno

--- plano-front/src/components/Dashboard/Horarios.vue
+++ plano-front/src/components/Dashboard/Horarios.vue
@@ -4,6 +4,45 @@
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 class="h2">Lista Horarios</h1>
             </div>
+            <template v-if="ccnSelecionado">
+                <h3>Ciência da Computação Noturno</h3>
+            </template>
+            <template v-if="!horarioVazio(CCN[0])">
+                <h5>1 Período</h5>
+                <b-table responsive :items="CCN[0]"></b-table>
+            </template>
+            <template v-if="!horarioVazio(CCN[1])">
+                <h5>2 Período</h5>
+                <b-table responsive :items="CCN[1]"></b-table>
+            </template>
+            <template v-if="!horarioVazio(CCN[2])">
+                <h5>3 Período</h5>
+                <b-table responsive :items="CCN[2]"></b-table>
+            </template>
+            <template v-if="!horarioVazio(CCN[3])">
+                <h5>4 Período</h5>
+                <b-table responsive :items="CCN[3]"></b-table>
+            </template>
+            <template v-if="!horarioVazio(CCN[4])">
+                <h5>5 Período</h5>
+                <b-table responsive :items="CCN[4]"></b-table>
+            </template>
+            <template v-if="!horarioVazio(CCN[5])">
+                <h5>6 Período</h5>
+                <b-table responsive :items="CCN[5]"></b-table>
+            </template>
+            <template v-if="!horarioVazio(CCN[6])">
+                <h5>7 Período</h5>
+                <b-table responsive :items="CCN[6]"></b-table>
+            </template>
+            <template v-if="!horarioVazio(CCN[7])">
+                <h5>8 Período</h5>
+                <b-table responsive :items="CCN[7]"></b-table>
+            </template>
+            <template v-if="!horarioVazio(CCN[8])">
+                <h5>9 Período</h5>
+                <b-table responsive :items="CCN[8]"></b-table>
+            </template>
 
         </div>
         <div class="col">
@@ -74,11 +113,37 @@
 <script>
     import _ from 'lodash'
 
-    const emptyPerfil = {
-        id: undefined,
-        nome: undefined,
-        abreviacao: undefined
-    }
+    const emptyHorarioNoturno = [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+                                 {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+                                ]
+
+    const emptyHorariosNoturnos = [[{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+        {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+    ],
+        [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+            {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+        ],
+        [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+            {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+        ],
+        [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+            {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+        ],
+        [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+            {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+        ],
+        [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+            {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+        ],
+        [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+            {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+        ],
+        [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+            {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+        ],
+        [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+            {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+        ]]
 
     export default {
         name: 'DashboardHorarios',
@@ -94,7 +159,35 @@
                     {text: "Eng. Comp.", value:4},
                 ],
                 rangeCCN: 1,
-                evenCCN:"false"
+                evenCCN:"false",
+                CCN:[[{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+                                 {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+                                ],
+                    [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+                                 {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+                                ],
+                    [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+                                 {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+                                ],
+                    [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+                                 {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+                                ],
+                    [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+                                 {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+                                ],
+                    [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+                                 {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+                                ],
+                    [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+                                 {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+                                ],
+                    [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+                                 {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+                                ],
+                    [{hora:"19-21",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined},
+                                 {hora:"21-23",seg: undefined, ter:undefined, qua:undefined, qui:undefined, sex:undefined, sab:undefined}
+                                ]]
+
             }
         },
 
@@ -115,33 +208,147 @@
             },
 
             createHorarios () {
+                this.CCN = _.clone(emptyHorariosNoturnos)
                 var grade
                 var inicio
                 var fim
+                var pedidos = _.filter(this.$store.state.pedido.Pedidos, ['Curso', 1])
+                var disciplinas = this.$store.state.disciplina.Disciplinas
                 var disciplinaGrades = this.$store.state.disciplinaGrade.DisciplinaGrades
                 var turmas = this.$store.state.turma.Turmas
                 //CC Noturno está selecionado
                 if(_.indexOf(this.cursos, 2)>-1) {
                     for (var i = 0; i < this.rangeCCN; i++) {
                         //grade
-                        grade = this.$refs.formCCN[i].value
+                        grade = this.$refs.formCCN[3*i].value
                         //inicio
-                        inicio = this.$refs.formCCN[i+1].value
+                        inicio = this.$refs.formCCN[3*i+1].value
                         //fim
-                        fim = this.$refs.formCCN[i+2].value
-
+                        fim = this.$refs.formCCN[3*i+2].value
                         for (var k = 0; k < disciplinaGrades.length; k++){
                             if((disciplinaGrades[k].Grade==grade) && (this.isEven(disciplinaGrades[k].periodo)==this.evenCCN) && (disciplinaGrades[k].periodo >= parseInt(inicio, 10)) && (disciplinaGrades[k].periodo<=parseInt(fim, 10))){
                                 for(var j = 0; j < turmas.length; j++){
-
                                     if(turmas[j].Disciplina==disciplinaGrades[k].Disciplina){
-                                        console.log([turmas[j].Horario1, turmas[j].Horario2])
+                                        for(var p = 0; p < pedidos.length; p++){
+                                            if((pedidos[p].vagasPeriodizadas>0)&&(pedidos[p].Turma==turmas[j].id)){
+                                                switch(turmas[j].Horario1){
+                                                    case 5:this.CCN[disciplinaGrades[k].periodo-1][0].seg = turmas[j].Disciplina
+                                                            console.log(this.CCN[disciplinaGrades[k].periodo-1][0].seg)
+                                                           break;
+                                                    case 6:this.CCN[disciplinaGrades[k].periodo-1][1].seg = turmas[j].Disciplina
+                                                            console.log(this.CCN[disciplinaGrades[k].periodo-1][1].seg)
+                                                           break;
+                                                    case 11:this.CCN[disciplinaGrades[k].periodo-1][0].ter = turmas[j].Disciplina
+                                                            console.log(this.CCN[disciplinaGrades[k].periodo-1][0].ter)
+                                                            break;
+                                                    case 12:this.CCN[disciplinaGrades[k].periodo-1][1].ter = turmas[j].Disciplina
+                                                            console.log(this.CCN[disciplinaGrades[k].periodo-1][1].ter)
+                                                            break;
+                                                    case 17:this.CCN[disciplinaGrades[k].periodo-1][0].qua = turmas[j].Disciplina
+                                                            console.log(this.CCN[disciplinaGrades[k].periodo-1][0].qua)
+                                                            break;
+                                                    case 18:this.CCN[disciplinaGrades[k].periodo-1][1].qua = turmas[j].Disciplina
+                                                            console.log(this.CCN[disciplinaGrades[k].periodo-1][1].qua)
+                                                            break;
+                                                    case 23:this.CCN[disciplinaGrades[k].periodo-1][0].qui = turmas[j].Disciplina
+                                                            console.log(this.CCN[disciplinaGrades[k].periodo-1][0].qui)
+                                                            break;
+                                                    case 24:this.CCN[disciplinaGrades[k].periodo-1][1].qui = turmas[j].Disciplina
+                                                            console.log(this.CCN[disciplinaGrades[k].periodo-1][1].qui)
+                                                            break;
+                                                    case 29:this.CCN[disciplinaGrades[k].periodo-1][0].sex = turmas[j].Disciplina
+                                                            console.log(this.CCN[disciplinaGrades[k].periodo-1][0].sex)
+                                                            break;
+                                                    case 30:this.CCN[disciplinaGrades[k].periodo-1][1].sex = turmas[j].Disciplina
+                                                            console.log(this.CCN[disciplinaGrades[k].periodo-1][1].sex)
+                                                            break;
+                                                    default: break;
+                                                }
+                                                switch(turmas[j].Horario2){
+                                                    case 5:this.CCN[disciplinaGrades[k].periodo-1][0].seg = turmas[j].Disciplina
+                                                        console.log(this.CCN[disciplinaGrades[k].periodo-1][0].seg)
+                                                        break;
+                                                    case 6:this.CCN[disciplinaGrades[k].periodo-1][1].seg = turmas[j].Disciplina
+                                                        console.log(this.CCN[disciplinaGrades[k].periodo-1][1].seg)
+                                                        break;
+                                                    case 11:this.CCN[disciplinaGrades[k].periodo-1][0].ter = turmas[j].Disciplina
+                                                        console.log(this.CCN[disciplinaGrades[k].periodo-1][0].ter)
+                                                        break;
+                                                    case 12:this.CCN[disciplinaGrades[k].periodo-1][1].ter = turmas[j].Disciplina
+                                                        console.log(this.CCN[disciplinaGrades[k].periodo-1][1].ter)
+                                                        break;
+                                                    case 17:this.CCN[disciplinaGrades[k].periodo-1][0].qua = turmas[j].Disciplina
+                                                        console.log(this.CCN[disciplinaGrades[k].periodo-1][0].qua)
+                                                        break;
+                                                    case 18:this.CCN[disciplinaGrades[k].periodo-1][1].qua = turmas[j].Disciplina
+                                                        console.log(this.CCN[disciplinaGrades[k].periodo-1][1].qua)
+                                                        break;
+                                                    case 23:this.CCN[disciplinaGrades[k].periodo-1][0].qui = turmas[j].Disciplina
+                                                        console.log(this.CCN[disciplinaGrades[k].periodo-1][0].qui)
+                                                        break;
+                                                    case 24:this.CCN[disciplinaGrades[k].periodo-1][1].qui = turmas[j].Disciplina
+                                                        console.log(this.CCN[disciplinaGrades[k].periodo-1][1].qui)
+                                                        break;
+                                                    case 29:this.CCN[disciplinaGrades[k].periodo-1][0].sex = turmas[j].Disciplina
+                                                        console.log(this.CCN[disciplinaGrades[k].periodo-1][0].sex)
+                                                        break;
+                                                    case 30:this.CCN[disciplinaGrades[k].periodo-1][1].sex = turmas[j].Disciplina
+                                                        console.log(this.CCN[disciplinaGrades[k].periodo-1][1].sex)
+                                                        break;
+                                                    default: break;
+                                                }
+
+                                            }
+                                        }
                                     }
                                 }
                             }
                         }
                     }
+                    for(var i=0; i<9;i++){
+                        for(var j=0; j<disciplinas.length;j++){
+                            if(this.CCN[i][0].seg == disciplinas[j].id){
+                                this.CCN[i][0].seg = disciplinas[j].codigo
+                            }
+                            if(this.CCN[i][1].seg == disciplinas[j].id){
+                                this.CCN[i][1].seg = disciplinas[j].codigo
+                            }
+                            if(this.CCN[i][0].ter == disciplinas[j].id){
+                                this.CCN[i][0].ter = disciplinas[j].codigo
+                            }
+                            if(this.CCN[i][1].ter == disciplinas[j].id){
+                                this.CCN[i][1].ter = disciplinas[j].codigo
+                            }
+                            if(this.CCN[i][0].qua == disciplinas[j].id){
+                                this.CCN[i][0].qua = disciplinas[j].codigo
+                            }
+                            if(this.CCN[i][1].qua == disciplinas[j].id){
+                                this.CCN[i][1].qua = disciplinas[j].codigo
+                            }
+                            if(this.CCN[i][0].qui == disciplinas[j].id){
+                                this.CCN[i][0].qui = disciplinas[j].codigo
+                            }
+                            if(this.CCN[i][1].qui == disciplinas[j].id){
+                                this.CCN[i][1].qui = disciplinas[j].codigo
+                            }
+                            if(this.CCN[i][0].sex == disciplinas[j].id){
+                                this.CCN[i][0].sex = disciplinas[j].codigo
+                            }
+                            if(this.CCN[i][1].sex == disciplinas[j].id){
+                                this.CCN[i][1].sex = disciplinas[j].codigo
+                            }
+                        }
+                    }
                 }
+                console.log(this.CCN)
+            },
+
+            ccnSelecionado(){
+              return (!(_.isEqual(this.CCN, emptyHorariosNoturnos)) && (_.indexOf(this.cursos, 2) > -1))
+            },
+
+            horarioVazio(horario){
+                return _.isEqual(horario, emptyHorarioNoturno)
             }
         },
 
