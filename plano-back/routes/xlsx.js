c7b7e775bc9a06aa13b12199c4af5d957d9780b6
lucascribeiro <lucascarvalho@ice.ufjf.br>
2019-03-19 18:01:56 -0300

    remove try

--- plano-back/routes/xlsx.js
+++ plano-back/routes/xlsx.js
@@ -5,8 +5,8 @@ const router = require('express').Router(),
 router.post('/', function(req, res, next){
     var wb = XLSX.utils.book_new();
     console.log(req.body)
-    var ws1 = try{XLSX.utils.table_to_sheet(req.body.table)}
-              catch(error){console.log(error)}
+    var ws1 = XLSX.utils.table_to_sheet(req.body.table)
+    console.log(ok)
     XLSX.utils.book_append_sheet(wb, ws1, "Sheet1");
     XLSX.writeFile(wb, 'tabelaPrincipal.xlsx')
     res.download('tabelaPrincipal.xlsx')
