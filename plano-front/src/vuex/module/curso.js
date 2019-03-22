c2a703b896c8bf22ec58b49df3f93bcd6ac9a332
lucascribeiro <lucascarvalho@ice.ufjf.br>
2019-03-22 19:05:39 -0300

    correção vuex curso

--- plano-front/src/vuex/module/curso.js
+++ plano-front/src/vuex/module/curso.js
@@ -10,19 +10,10 @@ const state = {
 const mutations = {
     [CURSO_FETCHED] (state, data) {
         state.Cursos = data.Cursos
-        if(localStorage.ativos){
-            state.Ativos = Object.assign({}, localStorage.ativos)
-        } else {
-            for (var c = 0; c < state.Cursos.length; c++) {
-                state.Ativos[state.Cursos[c].id] = true
-            }
-            state.Ativos = Object.assign({}, state.Ativos)
-        }
     },
 
     [SOCKET_CURSO_CREATED] (state, data) {
         state.Cursos.push(data[0].Curso)
-        Vue.set(state.Ativos, data[0].Curso.id, data[0].Curso)
     },
 
     [SOCKET_CURSO_UPDATED] (state, data) {
@@ -33,7 +24,6 @@ const mutations = {
     [SOCKET_CURSO_DELETED] (state, data) {
         let index = _.findIndex(state.Cursos, curso => curso.id === data[0].Curso.id);
         state.Cursos.splice(index, 1)
-        Vue.set(state.Ativos, data[0].Curso.id, undefined)
     },
 
 }
