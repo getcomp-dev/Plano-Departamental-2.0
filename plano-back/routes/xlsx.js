5d050715e67855d8ae8826b4d852746d68bf09ce
lucascribeiro <lucascarvalho@ice.ufjf.br>
2019-04-16 15:31:03 -0300

    Update xlsx.js

--- plano-back/routes/xlsx.js
+++ plano-back/routes/xlsx.js
@@ -12,118 +12,120 @@ router.post('/', function(req, res, next){
         horarios = models.Horario.findAll(),
         salas = models.Sala.findAll()
 
-    Promise.all([disciplinas, docentes, horarios, salas]).then(
+    Promise.all([disciplinas, docentes, horarios, salas]).then(function () {
 
         let data = []
         var header = ["S.", "Cod", "Disciplina", "C.", "Turma", "Horário", "Docente", "Turno", "Sala", "Total"]
-        if(cursos.length > 0){
-            cursos.forEach(function(curso) {header.push(curso.nome)
-                                            console.log(curso.nome)})
+        if (cursos.length > 0) {
+            cursos.forEach(function (curso) {
+                header.push(curso.nome)
+                console.log(curso.nome)
+            })
         }
         data.push(header)
-        if(turmas.length > 0){
-            turmas.forEach(function(turma) {
+        if (turmas.length > 0) {
+            turmas.forEach(function (turma) {
                 console.log(turma.letra)
                 let line = []
                 line.push(turma.periodo)
                 let disciplina = disciplinas.find(function (disc, index, array) {
-                    if(disc.id===turma.Disciplina)
+                    if (disc.id === turma.Disciplina)
                         return true
                     else
                         return false
                 })
-                if(disciplina !== undefined){
+                if (disciplina !== undefined) {
                     console.log(disciplina.nome)
                     line.push(disciplina.codigo)
                     line.push(disciplina.nome)
                     let carga = (disciplina.cargaTeorica + disciplina.cargaPratica)
                     line.push(carga)
-                }else{
+                } else {
                     line.push('')
                     line.push('')
                     line.push('')
                 }
                 line.push(turma.letra)
                 let horario1 = horarios.find(function (hora, index, array) {
-                    if(hora.id===turma.Horario1)
+                    if (hora.id === turma.Horario1)
                         return true
                     else
                         return false
                 })
                 let horario2 = horarios.find(function (hora, index, array) {
-                    if(hora.id===turma.Horario2)
+                    if (hora.id === turma.Horario2)
                         return true
                     else
                         return false
                 })
-                if(horario1===undefined){
-                    if(horario2 !== undefined){
+                if (horario1 === undefined) {
+                    if (horario2 !== undefined) {
                         line.push(horario2.horario)
                     }
-                    else{
+                    else {
                         line.push('')
                     }
-                }else{
-                    if(horario2===undefined){
+                } else {
+                    if (horario2 === undefined) {
                         line.push(horario1.horario)
-                    }else{
+                    } else {
                         line.push(horario1.horario + '/' + horario2.horario)
                     }
                 }
 
                 let docente = docentes.find(function (dcnt, index, array) {
-                    if(dcnt.id===turma.Docente)
+                    if (dcnt.id === turma.Docente)
                         return true
                     else
                         return false
                 })
-                if(docente === undefined){
+                if (docente === undefined) {
                     line.push('')
-                }else{
+                } else {
                     console.log(docente.apelido)
                     line.push(docente.apelido)
                 }
                 line.push(turma.turno)
 
                 let sala1 = salas.find(function (sl, index, array) {
-                    if(sl.id===turma.Sala1)
+                    if (sl.id === turma.Sala1)
                         return true
                     else
                         return false
                 })
                 let sala2 = salas.find(function (sl, index, array) {
-                    if(sl.id===turma.Sala2)
+                    if (sl.id === turma.Sala2)
                         return true
                     else
                         return false
                 })
-                if(sala1===undefined){
-                    if(sala2 !== undefined){
+                if (sala1 === undefined) {
+                    if (sala2 !== undefined) {
                         line.push(sala2.nome)
                     }
-                    else{
+                    else {
                         line.push('')
                     }
-                }else{
-                    if(sala2===undefined){
+                } else {
+                    if (sala2 === undefined) {
                         line.push(sala1.nome)
-                    }else{
+                    } else {
                         line.push(sala1.nome + '/' + sala2.nome)
                     }
                 }
                 let total = 0
                 let pds = []
-                cursos.forEach(function (curso){
-                    if(isArray(pedidos[turma.id])){
-                        let pedido = pedidos[turma.id].find(function(pd, index, array) {
-                            if(pd.Curso = curso.id)
+                cursos.forEach(function (curso) {
+                    if (isArray(pedidos[turma.id])) {
+                        let pedido = pedidos[turma.id].find(function (pd, index, array) {
+                            if (pd.Curso = curso.id)
                                 return true
                             else
                                 return false
                         })
                         pds.push(pedido.vagasPeriodizadas + '/' + pedido.vagasNaoPeriodizadas)
                         total = total + pedido.vagasPeriodizadas + pedido.vagasNaoPeriodizadas
-                    }else{
+                    } else {
                         pd.push('')
                     }
                 })
@@ -137,7 +139,7 @@ router.post('/', function(req, res, next){
         XLSX.utils.book_append_sheet(wb, ws, "DCC")
         XLSX.writeFile(wb, 'tabelaPrincipal.xlsx')
         res.download('tabelaPrincipal.xlsx')
-        )
+    })
 
 })
 
