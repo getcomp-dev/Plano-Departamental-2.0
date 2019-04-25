315b7a0e074d08410f0d234884f8045ef5dc5c6c
lucascribeiro <lucascarvalho@ice.ufjf.br>
2019-04-25 17:16:52 -0300

    Update xlsx.js

--- plano-back/routes/xlsx.js
+++ plano-back/routes/xlsx.js
@@ -269,6 +269,7 @@ router.post('/', function(req, res, next){
                                     return false
                             })
                             if(pedido){
+                                console.log(curso.id + ' ' + pedido.Curso)
                                 pds.push(pedido.vagasPeriodizadas + '/' + pedido.vagasNaoPeriodizadas)
                                 total = total + pedido.vagasPeriodizadas + pedido.vagasNaoPeriodizadas
                             }else{
