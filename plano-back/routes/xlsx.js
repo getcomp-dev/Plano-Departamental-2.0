94838d3d8164a498f71870824895cc03014d2800
lucascribeiro <lucascarvalho@ice.ufjf.br>
2019-04-25 18:04:00 -0300

    Update xlsx.js

--- plano-back/routes/xlsx.js
+++ plano-back/routes/xlsx.js
@@ -301,9 +301,12 @@ router.post('/', function(req, res, next){
         let ws = XLSX.utils.aoa_to_sheet(data)
         XLSX.utils.book_append_sheet(wb, ws, "DCC")
         XLSX.writeFile(wb, 'tabelaPrincipal.xlsx')
-        res.download('/home/planodcc/Plano-Departamental-2.0/plano-back/tabelaPrincipal.xlsx', 'tabelas.xlsx')
     })
 
+})
+
+router.post('/download', function(req, res, next){
+    res.download('/home/planodcc/Plano-Departamental-2.0/plano-back/tabelaPrincipal.xlsx', 'tabelas.xlsx')
 })
 
 
