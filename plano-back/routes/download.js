55cc808221525ea223e7becadd7d686ec64a42c1
lucascribeiro <lucascarvalho@ice.ufjf.br>
2019-05-02 20:19:11 -0300

    Update download.js

--- plano-back/routes/download.js
+++ plano-back/routes/download.js
@@ -14,6 +14,22 @@ router.get('/', function(req, res, next){
     console.log('Tabela Lida')
     zip.file("Tabelas.xlsx", tabela)
     console.log('Tabela adicionada ao zip')
+    fs.writeFileSync('Cargas.pdf', req.body.pdfCarga)
+    console.log('arquivo .pdf criado')
+    let carga = fs.readFileSync('./Cargas.pdf', (err, data) => {
+        if(err) throw err
+        return data
+    })
+    zip.file("Cargas.pdf", carga)
+    console.log('pdf adicionado ao .zip')
+    fs.writeFileSync('Labs.pdf', req.body.pdfLabs)
+    console.log('arquivo .pdf criado')
+    let labs = fs.readFileSync('./Labs.pdf', (err, data) => {
+        if(err) throw err
+        return data
+    })
+    zip.file("Labs.pdf", labs)
+    console.log('pdf adicionado ao .zip')
     console.log('criando sql')
     fs.open('backup.sql', 'w', function (err, file) {
         if (err) throw err;
