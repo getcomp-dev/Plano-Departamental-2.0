7061701d8d25f0945dcf6d5565ad2d4381cac20e
lucascribeiro <lucascarvalho@ice.ufjf.br>
2019-07-08 16:54:47 -0300

    versão funcional tela de grades das disciplinas

--- plano-front/src/components/Dashboard/GradeDisciplinas.vue
+++ plano-front/src/components/Dashboard/GradeDisciplinas.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="Grades Disciplinas" style="height: calc(100vh - 48px)" v-if="Admin">
         <div class="d-flex center-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom" style="overflow: auto; width: 100%">
-            <h1 class="h2 col-12">Grades Disciplinas</h1>
+            <h1 class="h2 col-11">Grades Disciplinas</h1>
+            <div class="col-1">
+                <b-form-input v-model="novoAno" v-on:blur="runNovoAno" style="width:60px !important; height: 30px; float: right; position:relative; text-align: center"></b-form-input>
+            </div>
         </div>
 
         <div style="height: 80vh; overflow-y: scroll; overflow-x: scroll;" ref = "mainTable">
             <table class="table table-hover table-sm">
                 <thead class="thead-light">
                 <tr>
-                    <th scope="col"><p style="text-align: center">Cod</p></th>
-                    <th scope="col"><p style="text-align: center">Disciplina</p></th>
-                    <th scope="col"><p style="text-align:center">76A</p></th>
-                    <th scope="col"><p style="text-align:center">35A</p></th>
-                    <th scope="col"><p style="text-align:center">65C</p></th>
-                    <th scope="col"><p style="text-align:center">65B</p></th>
+                    <th scope="col" ><p style="text-align: center">Cod</p></th>
+                    <th scope="col" style="width: 400px"><p style="text-align: center">Disciplina</p></th>
+                    <th scope="col" style="width: 135px"><p style="text-align:center">76A</p></th>
+                    <th scope="col" style="width: 135px"><p style="text-align:center">35A</p></th>
+                    <th scope="col" style="width: 135px"><p style="text-align:center">65C</p></th>
+                    <th scope="col" style="width: 135px"><p style="text-align:center">65B</p></th>
                 </tr>
                 </thead>
                 <tbody>
@@ -22,12 +25,37 @@
                         <tr v-for="disciplina in inPerfil(perfil, Disciplinas)" :key="disciplina.id"  v-bind:class="{'basico':perfil.id==1,'avancado':perfil.id==2, 'arqso':perfil.id==3,
                  'bancosdedados':perfil.id==4, 'computacaografica':perfil.id==5, 'engenhariasoftware':perfil.id==6, 'iaic':perfil.id==7, 'numoc':perfil.id==8, 'redes':perfil.id==9, 'teoria':perfil.id==10,
                  'humempre':perfil.id==11, 'multi': perfil.id==12, 'ice':perfil.id==13}">
-                            <td><p style="text-align: center">{{disciplina.codigo}}<p/></td>
-                            <td><p>{{disciplina.nome}}</p></td>
-                            <td style="border-style: solid; border-width: 1px; border-color: whitesmoke;"><p style=" text-align: center"> - </p></td>
-                            <td style="border-style: solid; border-width: 1px; border-color: whitesmoke;"><p style=" text-align: center"> - </p></td>
-                            <td style="border-style: solid; border-width: 1px; border-color: whitesmoke;"><p style=" text-align: center"> - </p></td>
-                            <td style="border-style: solid; border-width: 1px; border-color: whitesmoke;"><p style=" text-align: center"> - </p></td>
+                            <td style="border-style: solid; border-width: 1px; border-color: whitesmoke;"><p style="text-align: center">{{disciplina.codigo}}<p/></td>
+                            <td style="border-style: solid; border-width: 1px; border-color: whitesmoke;"><p>{{disciplina.nome}}</p></td>
+                            <td style="border-style: solid; border-width: 1px; border-color: whitesmoke;"><p style=" text-align: center">
+                                <template v-if="disciplinasGrades[disciplina.id][2][0] != 0 && disciplinasGrades[disciplina.id][2][1] != 0"> 1º Semestre: {{disciplinasGrades[disciplina.id][2][0]}}º Período / 2º Semestre: {{disciplinasGrades[disciplina.id][2][1]}}º Período </template>
+                                <template v-else>
+                                    <template v-if="disciplinasGrades[disciplina.id][2][0] != 0"> 1º Semestre: {{disciplinasGrades[disciplina.id][2][0]}}º Período </template>
+                                    <template v-if="disciplinasGrades[disciplina.id][2][1] != 0"> 2º Semestre: {{disciplinasGrades[disciplina.id][2][1]}}º Período </template>
+                                </template>
+                            </p></td>
+                            <td style="border-style: solid; border-width: 1px; border-color: whitesmoke;"><p style=" text-align: center">
+                                <template v-if="disciplinasGrades[disciplina.id][0][0] != 0 && disciplinasGrades[disciplina.id][0][1] != 0"> 1º Semestre: {{disciplinasGrades[disciplina.id][0][0]}}º Período / 2º Semestre: {{disciplinasGrades[disciplina.id][0][1]}}º Período </template>
+                                <template v-else>
+                                    <template v-if="disciplinasGrades[disciplina.id][0][0] != 0"> 1º Semestre: {{disciplinasGrades[disciplina.id][0][0]}}º Período </template>
+                                    <template v-if="disciplinasGrades[disciplina.id][0][1] != 0"> 2º Semestre: {{disciplinasGrades[disciplina.id][0][1]}}º Período </template>
+                                </template>
+                            </p></td>
+                            <td style="border-style: solid; border-width: 1px; border-color: whitesmoke;"><p style=" text-align: center">
+                                <template v-if="disciplinasGrades[disciplina.id][3][0] != 0 && disciplinasGrades[disciplina.id][3][1] != 0"> 1º Semestre: {{disciplinasGrades[disciplina.id][3][0]}}º Período / 2º Semestre: {{disciplinasGrades[disciplina.id][3][1]}}º Período </template>
+                                <template v-else>
+                                    <template v-if="disciplinasGrades[disciplina.id][3][0] != 0"> 1º Semestre: {{disciplinasGrades[disciplina.id][3][0]}}º Período </template>
+                                    <template v-if="disciplinasGrades[disciplina.id][3][1] != 0"> 2º Semestre: {{disciplinasGrades[disciplina.id][3][1]}}º Período </template>
+                                </template>
+                            </p></td>
+                            <td style="border-style: solid; border-width: 1px; border-color: whitesmoke;"><p style=" text-align: center">
+                                <template v-if="disciplinasGrades[disciplina.id][1][0] != 0 && disciplinasGrades[disciplina.id][1][1] != 0"> 1º Semestre: {{disciplinasGrades[disciplina.id][1][0]}}º Período / 2º Semestre: {{disciplinasGrades[disciplina.id][1][1]}}º Período </template>
+                                <template v-else>
+                                    <template v-if="disciplinasGrades[disciplina.id][1][0] != 0"> 1º Semestre: {{disciplinasGrades[disciplina.id][1][0]}}º Período </template>
+                                    <template v-if="disciplinasGrades[disciplina.id][1][1] != 0"> 2º Semestre: {{disciplinasGrades[disciplina.id][1][1]}}º Período </template>
+                                </template>
+                            </p></td>
+
 
                         </tr>
                     </template>
@@ -48,37 +76,142 @@
         data () {
             return {
                 error: undefined,
+                ano: undefined,
+                novoAno: undefined,
                 disciplinasGrades: {},
+                grades:{
+                    1: undefined, //CCN
+                    2: undefined, //EC
+                    3: undefined, //SI
+                    4: undefined  //CCD
+                },
                 gradesAtivas:{
-                   1: undefined, //CCN
-                   2: undefined, //EC
-                   3: undefined, //SI
-                   4: undefined  //CCD
+                   1: [], //CCN
+                   2: [], //EC
+                   3: [], //SI
+                   4: []  //CCD
                 }
             }
         },
 
         beforeMount: function () {
-            this.$store.state.disciplina.Disciplinas.forEach( d => {
-                this.disciplinasGrades[d.id] =[[0, 0], [0, 0], [0, 0], [0, 0]]
-            })
-
-            for(let i = 1; i <= 4; i++){
-                this.gradesAtivas[i] = _.orderBy(_.filter(this.$store.state.grade.Grades, ['Curso', i]), 'periodoInicio', 'desc')
-            }
-
-            console.log(this.disciplinasGrades)
-            console.log(this.gradesAtivas)
+            this.ano = this.$store.state.plano.Plano[0].ano
+            this.novoAno = this.ano
+            this.runAll()
         },
 
         methods: {
 
-            inPerfil: function (perfil, disciplinas) {
+            inPerfil: function (perfil, disciplinas) {//Verifica se uma disciplina faz parte de um perfil
                 return disciplinas.filter(function (disciplina) {
                     return disciplina.Perfil===perfil.id
                 })
 
             },
+
+            runNovoAno: function () {//executa runAll, modificando o ano
+              if(this.ano != this.novoAno){
+                  this.ano = this.novoAno
+                  this.runAll()
+              }
+            },
+
+            runAll: function () {//cria objeto para armazenar os períodos das disciplinas e chama as funções que a populam
+                this.$store.state.disciplina.Disciplinas.forEach( d => {
+                    this.disciplinasGrades[d.id] = [[0, 0], [0, 0], [0, 0], [0, 0]]//inisializa os períodos em 0 [Primeiro Semestre, Segundo Semestre]
+                })
+                this.getGrades()
+                this.get1Periodo()
+                this.get2Periodo()
+            },
+
+            getGrades: function () {//popula as grades disponíveis de cada curso em um objeto
+                for(let i = 1; i <= 4; i++){
+                    this.grades[i] = _.orderBy(_.filter(this.$store.state.grade.Grades, ['Curso', i]), 'periodoInicio', 'desc')
+                }
+            },
+
+            get1Periodo: function () {//Armazena os períodos de cada disciplina no primeiro semestre
+                //retorna lista com os ids das disciplinas
+                let disciplinas = Object.keys(this.disciplinasGrades)
+                //marca o último período em que uma grade é válida
+                for(let i = 1; i <= 4; i++){
+                    this.grades[i].forEach(g => {
+                        let fim = 1 + 2*(this.ano - parseInt(g.periodoInicio.slice(0,4), 10)) + ((1 - parseInt(g.periodoInicio.slice(5,6), 10))/2)
+                        if(fim > 10){
+                            fim = 10
+                        }
+                        this.gradesAtivas[i].push({grade: g, fim:fim})
+                    })
+                    if(this.gradesAtivas[i][this.gradesAtivas[i].length - 1].fim !== 10){
+                        this.gradesAtivas[i][this.gradesAtivas[i].length - 1].fim = 10
+                    }
+                }
+
+                disciplinas.forEach(d => {
+                    for(let i = 1; i <= 4; i++){//ids dos cursos de computação
+                        let gradedisciplina
+                        let curso = _.find(this.$store.state.curso.Cursos, ['id', i])
+                        for(let j = 0; j < this.gradesAtivas[i].length; j++){//itera pelas grades de um curso, começando da mais recente (lista ordenada)
+                            gradedisciplina = _.find(this.$store.state.disciplinaGrade.DisciplinaGrades, { 'Disciplina': parseInt(d, 10), 'Grade':this.gradesAtivas[i][j].grade.id})//Encontra a disciplina nas grades da computação, começando pela mais recente. Caso não encontre, retorna undefined
+                            if(gradedisciplina !== undefined){//verifica se a disciplina está na grade procurada
+                                if(gradedisciplina.periodo < this.gradesAtivas[i][j].grade.fim){//verifica se essa grade está sendo usada para essa disciplina(limite superior)
+                                    if((j > 0) && (this.gradesAtivas[i][j].grade.fim < gradedisciplina.periodo) || j===0)//verifica se a disciplina não deveria estar incluída na grade anterior, ou se essa é a grade mais recente(limite inferior)
+                                        break;//caso essa seja a grade correta, encerra o loop nas grades
+                                    else
+                                        gradedisciplina = undefined//necessário caso seja a última iteração do loop
+                                }
+                            }
+                        }if(gradedisciplina !== undefined){
+                            if((curso.semestreInicial === 1 || curso.semestreInicial === 3) &&(gradedisciplina.periodo % 2 === 1))//tem entrada de alunos no 1º semestre ou no 3º semestre -> terá períodos ímpares no primeiro semestre
+                                this.disciplinasGrades[parseInt(d, 10)][i - 1][0] = gradedisciplina.periodo
+                            if((curso.semestreInicial === 2 || curso.semestreInicial === 3) &&(gradedisciplina.periodo % 2 === 0))//tem entrada de alunos no 2º semestre ou no 3º semestre -> terá períodos pares no primeiro semestre
+                                this.disciplinasGrades[parseInt(d, 10)][i - 1][0] = gradedisciplina.periodo
+                        }
+                    }
+                })
+            },
+
+            get2Periodo: function () {//Armazena os períodos de cada disciplina no segundo semestre
+                //retorna lista com os ids das disciplinas
+                let disciplinas = Object.keys(this.disciplinasGrades)
+                //marca o último período em que uma grade é válida
+                for(let i = 1; i <= 4; i++){
+                    this.grades[i].forEach(g => {
+                        let fim = 1 + 2*(this.ano - parseInt(g.periodoInicio.slice(0,4), 10)) + ((3 - parseInt(g.periodoInicio.slice(5,6), 10))/2)
+                        if(fim > 10){
+                            fim = 10
+                        }
+                        this.gradesAtivas[i].push({grade: g, fim:fim})
+                    })
+                    if(this.gradesAtivas[i][this.gradesAtivas[i].length - 1].fim !== 10){
+                        this.gradesAtivas[i][this.gradesAtivas[i].length - 1].fim = 10
+                    }
+                }
+
+                disciplinas.forEach(d => {
+                    for(let i = 1; i <= 4; i++){//ids dos cursos de computação
+                        let gradedisciplina
+                        let curso = _.find(this.$store.state.curso.Cursos, ['id', i])
+                        for(let j = 0; j < this.gradesAtivas[i].length; j++){//itera pelas grades de um curso, começando da mais recente (lista ordenada)
+                            gradedisciplina = _.find(this.$store.state.disciplinaGrade.DisciplinaGrades, { 'Disciplina': parseInt(d, 10), 'Grade':this.gradesAtivas[i][j].grade.id})//Encontra a disciplina nas grades da computação, começando pela mais recente. Caso não encontre, retorna undefined
+                            if(gradedisciplina !== undefined){//verifica se a disciplina está na grade procurada
+                                if(gradedisciplina.periodo < this.gradesAtivas[i][j].grade.fim){//verifica se essa grade está sendo usada para essa disciplina(limite superior)
+                                    if((j > 0) && (this.gradesAtivas[i][j].grade.fim < gradedisciplina.periodo) || j===0)//verifica se a disciplina não deveria estar incluída na grade anterior, ou se essa é a grade mais recente(limite inferior)
+                                        break;//caso essa seja a grade correta, encerra o loop nas grades
+                                    else
+                                        gradedisciplina = undefined//necessário caso seja a última iteração do loop
+                                }
+                            }
+                        }if(gradedisciplina !== undefined){
+                            if((curso.semestreInicial === 1 || curso.semestreInicial === 3) &&(gradedisciplina.periodo % 2 === 0))//tem entrada de alunos no 1º semestre ou no 3º semestre -> terá períodos pares no segundo semestre
+                                this.disciplinasGrades[parseInt(d, 10)][i - 1][1] = gradedisciplina.periodo
+                            if((curso.semestreInicial === 2 || curso.semestreInicial === 3) &&(gradedisciplina.periodo % 2 === 1))//tem entrada de alunos no 2º semestre ou no 3º semestre -> terá períodos ímpares no segundo semestre
+                                this.disciplinasGrades[parseInt(d, 10)][i - 1][1] = gradedisciplina.periodo
+                        }
+                    }
+                })
+            }
 
 
 
@@ -209,6 +342,11 @@
 
     p {
         margin: 0px;
+    }
+
+    th {
+        position: sticky;
+        top: -1px;
     }
 
 </style>
