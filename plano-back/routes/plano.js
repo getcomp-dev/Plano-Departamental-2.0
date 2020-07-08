d6cc719b276e49d50426afca732a48d0955145c1
lucascribeiro <lucascarvalho@ice.ufjf.br>
2020-07-08 11:04:44 -0300

    Delete Plano adicionado ao back-end

--- plano-back/routes/plano.js
+++ plano-back/routes/plano.js
@@ -4,6 +4,20 @@ const models = require('../models/index'),
     SM = require('../library/SocketMessages'),
     CustomError = require('../library/CustomError')
 
+const history = function(params){
+    models.History.create({
+        tabelaModificada: 'Plano',
+        campoModificado: params.fieldName,
+        linhaModificada: params.lineId,
+        valorAnterior: params.oldValue,
+        valorNovo: params.newValue,
+        tipoOperacao: params.operationType,
+        usuario: params.user
+    }).then(function (history) {
+        ioBroadcast(SM.HISTORY_CREATED, {'msg': 'Log atualizado', 'History': history})
+    })
+}
+
 router.get('/', function (req, res, next) {
     models.Plano.findAll().then(function (plano) {
         res.send({
@@ -56,6 +70,33 @@ router.post('/:ano([0-9]+)', function (req, res, next) {
         res.send({
             success: true,
             message: 'Plano atualizado!',
+            Plano: plano
+        })
+    }).catch(function (err) {
+        return next(err, req, res)
+    })
+})
+
+router.delete('/:id([0-9]+)', function (req, res, next) {
+    console.log('\nRequest de '+req.usuario.nome+'\n')
+    models.Plano.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then(function (plano) {
+        if (!plano)
+            throw new CustomError(400, 'Plano inválido')
+
+        return plano.destroy()
+    }).then(function (plano) {
+        ioBroadcast(SM.PLANO_DELETED, {'msg': 'Plano excluído!', 'Plano': plano})
+        console.log('\nRequest de '+req.usuario.nome+'\n')
+
+        history({operationType: "Delete", user: req.usuario.nome, lineId: `${plano.ano}`})
+
+        res.send({
+            success: true,
+            message: 'Plano excluído',
             Plano: plano
         })
     }).catch(function (err) {
