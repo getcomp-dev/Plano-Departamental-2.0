689e21519fb5df19bc932b78c8b12464d933993d
lucascribeiro <lucascarvalho@ice.ufjf.br>
2021-03-02 13:57:08 -0300

    Teste Child Process

---
+++ plano-back/library/childProcesses.js
@@ -0,0 +1,89 @@
+const models = require('../models/index');
+const { Op } = require("sequelize");
+
+var table = process.argv[2];
+
+const operations = {
+    turma: function(form){
+        models.Turma.create({
+            periodo: form.periodo,
+            letra: form.letra,
+            turno1:  form.turno1,
+            turno2: form.turno2,
+            Disciplina: form.Disciplina,
+            Docente1: form.Docente1,
+            Docente2: form.Docente2,
+            Horario1: form.Horario1,
+            Horario2: form.Horario2,
+            Sala1: form.Sala1,
+            Sala2: form.Sala2,
+            Plano: form.Plano
+        }).then(
+            function(turma) {
+                process.send({result: turma});
+                process.disconnect();
+            }
+        )
+    },
+    turmaExterna: function(form){
+        models.TurmaExterna.create({
+            periodo: form.periodo,
+            letra: form.letra,
+            turno1:  form.turno1,
+            turno2: form.turno2,
+            Disciplina: form.Disciplina,
+            Horario1: form.Horario1,
+            Horario2: form.Horario2,
+            Sala1: form.Sala1,
+            Sala2: form.Sala2,
+            Plano: form.Plano
+        }).then(
+            function(turma) {
+                process.send({turma: turma, success: true});
+                process.disconnect();
+            }
+        ).catch(
+            function(err) {
+                process.send({err: err, success: false});
+                process.disconnect();
+            }
+        )
+
+    },
+    pedido: function(form){
+        models.Pedido.update({
+            vagasPeriodizadas: form.vagasPeriodizadas,
+            vagasNaoPeriodizadas: form.vagasNaoPeriodizadas,
+            editado1: form.editado1,
+            editado2: form.editado2,
+
+        }, {
+            where:{
+                Curso: form.Curso,
+                Turma: form.Turma
+            }
+        }).then(
+            function(pedido) {
+                process.send({result: pedido});
+                process.disconnect();
+            }
+        )
+    },
+    pedidoExterno: function(form){
+        pedido.updateAttributes({
+            vagasPeriodizadas: form.vagasPeriodizadas,
+            vagasNaoPeriodizadas: form.vagasNaoPeriodizadas,
+            Curso: form.Curso,
+            Turma: form.Turma
+        }).then(
+            function(pedido) {
+                process.send({result: pedido});
+                process.disconnect();
+            }
+        )
+    }
+};
+
+process.on('message', function(form){
+    operations[table](form);
+});
